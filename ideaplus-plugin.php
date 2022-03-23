3b9e945c304a2b059e972efd4dedcf7374a28fcb
yulingwei <767502630@qq.com>
2022-03-23 17:52:01 +0800

    修复问题

--- ideaplus-plugin.php
+++ ideaplus-plugin.php
@@ -27,20 +27,7 @@
 if (!defined('WPINC')) {
     die;
 }
-function getCurPageURL() {
-    $pageURL = 'http';
-    if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
-        $pageURL .= "s";
-    }
-    $pageURL .= "://";
-    if ($_SERVER["SERVER_PORT"] != "80") {
-        $pageURL .= $_SERVER["SERVER_NAME"].
-        ":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-    } else {
-        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-    }
-    return $pageURL;
-}
+
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
